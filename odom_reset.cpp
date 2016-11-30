c4ae8ccd290a8f9253b4e0b5b3efcfe471640169
Hussain <Zaki-@users.noreply.github.com>
2016-11-30 15:52:55 -0500

    Update odom_reset.cpp

--- odom_reset.cpp
+++ odom_reset.cpp
@@ -1,1 +1,14 @@
 
+#include "ros/ros.h"
+#include "std_msgs/Empty.h"
+
+
+void odom_reset()
+{
+  ros::init(argc, argv, "odom_reset");
+  ros::NodeHandle n;
+  std_msgs::Empty mymsg;
+  ros::Publisher resetOdometry = n.advertise<std_msgs::Empty>("/mobile_base/commands/reset_odometry", 1000, true);
+  resetOdometry.publish(mymsg); 
+  
+}
